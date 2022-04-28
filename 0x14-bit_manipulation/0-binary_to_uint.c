4167b698e330ce0c030508dfeadb5bb14055badd
speak2debby <speak2debby@gmail.com>
2022-04-28 22:15:39 +0100

    bit manipulation in C programming

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,9 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
- *
  * Return: the converted number
  */
 unsigned int binary_to_uint(const char *b)
@@ -20,6 +19,5 @@ unsigned int binary_to_uint(const char *b)
 			return (0);
 		dec_val = 2 * dec_val + (b[i] - '0');
 	}
-
 	return (dec_val);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * print_binary - prints the binary equivalent of a decimal number
- * @n: number to print in binary
+ * print_binary - prints the binary representation of a number
+ * @n: parameter
  */
 void print_binary(unsigned long int n)
 {
